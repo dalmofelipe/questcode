d9be270b36b527d14db17fec8fafaa9f922bf4a6
Dalmo Felipe <dalmo.felipe@gmail.com>
2023-06-02 17:06:00 -0300

    jenkins file pipeline example runing cmds in custom docker caontainer

--- devops/jenkins/02-pipeline-exemplo.groovy
+++ devops/jenkins/02-pipeline-exemplo.groovy
@@ -5,9 +5,11 @@ podTemplate(
     namespace: 'devops', 
     containers: [
         containerTemplate(
+            name: 'docker-container', 
+            image: 'docker', 
             args: 'cat', 
             command: '/bin/sh -c', 
-            image: 'docker', 
+            ttyEnabled: true, 
             livenessProbe: containerLivenessProbe(
                 execArgs: '', 
                 failureThreshold: 0, 
@@ -16,16 +18,15 @@ podTemplate(
                 successThreshold: 0, 
                 timeoutSeconds: 0
             ), 
-            name: 'docker-container', 
             resourceLimitCpu: '', 
             resourceLimitEphemeralStorage: '', 
             resourceLimitMemory: '', 
             resourceRequestCpu: '', 
             resourceRequestEphemeralStorage: '', 
             resourceRequestMemory: '', 
-            ttyEnabled: true, 
             workingDir: '/home/jenkins/agent'
         )
+        // containerTemplate(name: 'nodejs')
     ],
     volumes: [
         hostPathVolume(
@@ -38,13 +39,17 @@ podTemplate(
 {
     // este node será executado no podTemplate 'questcode'
     node('questcode') { 
-        stage('Checkout') {
+        stage('Build') {
             echo 'Clone do repositório do código no Github'
             sh 'ls -ltra'
         }
-        stage('Build') {
-            echo 'Executando NPM install'
-            sh 'ls -ltra'
+        stage('Package') {
+            container('docker-container') { // os comandos serão executados no container especificado
+                echo 'comunicando docker k8s com docker do host'
+                echo 'listando imagens do host'
+                sh 'docker images'
+                sh 'ls -ltra'
+            }
         }
         stage('Deploy') {
             echo 'Iniciando deploy com Helm'
